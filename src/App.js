e37c1a51f6f9c5f7b6e4a621431b8d883e58cf1c
zdurr22 <zdurr22@gmail.com>
2021-11-01 12:07:18 -0400

    commit tree 4cba7466d8c0c8a6e9612cc53d8c9ec9b94fb21c

--- src/App.js
+++ src/App.js
@@ -1,1 +1,15 @@
 // create your App component here
+
+import { Component } from "react";
+
+export default class App extends Component {
+
+    render() {
+        
+    }
+    componentDidMount() {
+        fetch('http://api.open-notify.org/astros.json')
+        .then((response) => response.json())
+
+    }
+}
